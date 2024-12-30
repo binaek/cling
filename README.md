943e80631944cbfa71ef2702ceb9415aeeb6a588
Binaek Sarkar <binaek89@gmail.com>
2024-12-30 13:53:24 +0530

    readme

--- README.md
+++ README.md
@@ -1,3 +1,136 @@
-# cling
+# CLIng is a command-line parser for Go
 
-A Simple and Clear CLI library.
+The aim of CLIng is to support arbitrarily complex command line flags and arguments for Go Programs.
+
+Command line inputs are expressed as Go annotated `structs` that are hydrated at runtime using the generic `cling.Hydrate` function.
+
+## Usage
+
+```go
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/binaek/cling"
+	"github.com/pkg/errors"
+)
+
+// Version of the program
+var version = "0.0.1-dev.0"
+
+func main() {
+	// create a context so that we can shut things down gracefully when we receive a SIGNAL
+	// we can pass this context to the CLI and it will be used for all downstream calls
+	ctx, _ := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGKILL)
+
+	cli := setupCLI(ctx, version)
+	if err := cli.Run(ctx, os.Args); err != nil {
+		fmt.Println("Error:", err)
+	}
+}
+
+type startCmdInput struct {
+	AnyArbitraryName     string `cling-name:"string-input"`
+	AnotherArbitraryName int    `cling-name:"int-input"`
+}
+
+// This is the handler for the `start` command.
+// It will be called when the `start` command is executed in the CLI.
+// The `args` parameter will contain the arguments passed to the command without the executable.
+func startCmdHandler(ctx context.Context, args []string) error {
+	input := startCmdInput{}
+
+	// Hydrate the input struct with the arguments passed to the command
+	// This will populate the input struct with the values of the flags passed to the command
+	if err := cling.Hydrate(ctx, args, &input); err != nil {
+		return errors.Wrap(err, "failed to hydrate input arguments")
+	}
+
+	fmt.Println("String input:", input.AnyArbitraryName)
+	fmt.Println("Int input:", input.AnotherArbitraryName)
+
+	return nil
+}
+
+func setupCLI(ctx context.Context, version string) *cling.CLI {
+	cli := cling.NewCLI("program", version).
+		WithDescription("This is an example go program").
+		WithCommand(
+			cling.NewCommand("start", startCmdHandler).
+				WithDescription("Start the program").
+				WithFlag(
+					cling.
+						NewStringCmdInput("string-input"). // MUST match with the a field in the input struct
+						Required().                        // Make this flag required
+						WithDescription("A string input"). // Give a description for help content generation
+						AsFlag(),                          // This is important to tell cling that this is a flag
+				).
+				WithFlag(
+					cling.
+						NewIntCmdInput("int-input").    // MUST match with the a field in the input struct
+						WithDefault(1000).              // Set a default value - this is not required
+						WithDescription("A INT input"). // Give a description for help content generation
+						AsFlag(),                       // This is important to tell cling that this is a flag
+				),
+		)
+	return cli
+}
+
+```
+
+## Features
+
+### Version
+
+CLIng provides a `--version` flag that can be used to show the version of the CLI.
+
+```shell
+$ program --version
+program version 0.0.1-dev.0
+```
+
+```shell
+$ program start --version
+program version 0.0.1-dev.0
+```
+
+### Help
+
+CLIng provides a `--help` flag that can be used to show help content for the CLI or it's commands. This is automatically generated based on the input struct and the flags that are defined.
+
+#### Help for the CLI
+
+When the `--help` flag is specified, the help content is generated for the CLI and it's commands.
+
+```shell
+$ program --help
+Usage: program [command] [flags] [arguments]
+
+Available Commands:
+  start
+
+Flags:
+  --help        Show help information
+  --version     Show version information
+
+Use "program [command] --help" for more information about a command.
+```
+
+#### Help for a Command
+
+When the `--help` flag is specified with a command, the help content is generated for the command with it's flags and arguments.
+
+```shell
+$ program start --help
+Usage: program start [flags] [arguments]
+
+Flags:
+  --string-input string   A string input
+  --int-input int         A INT input
+
+```
---
+++ _examples/basic/main.go
@@ -0,0 +1,187 @@
+package cling
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"testing"
+)
+
+type Config struct {
+	Positional1 string   `cling-name:"positional1"`
+	Positional2 string   `cling-name:"positional2"`
+	Positional3 string   `cling-name:"positional3"`
+	StringFlag1 string   `cling-name:"stringflag1"`
+	IntFlag1    int      `cling-name:"intflag1"`
+	SliceFlag1  []int    `cling-name:"sliceflag1"`
+	SliceFlag2  []string `cling-name:"sliceflag2"`
+	SliceFlag3  []string `cling-name:"sliceflag3"`
+	SliceFlag4  []int    `cling-name:"sliceflag4"`
+}
+
+func (c *Config) String() string {
+	var sb strings.Builder
+
+	sb.WriteString("Config{\n")
+	sb.WriteString(fmt.Sprintf("  Positional1: %s\n", c.Positional1))
+	sb.WriteString(fmt.Sprintf("  Positional2: %s\n", c.Positional2))
+	sb.WriteString(fmt.Sprintf("  Positional3: %s\n", c.Positional3))
+	sb.WriteString(fmt.Sprintf("  StringFlag1: %s\n", c.StringFlag1))
+	sb.WriteString(fmt.Sprintf("  IntFlag1: %d\n", c.IntFlag1))
+	sb.WriteString("}")
+
+	return sb.String()
+}
+
+func action(ctx context.Context, args []string) error {
+	cfg := &Config{}
+	if err := Hydrate(ctx, args, cfg); err != nil {
+		return err
+	}
+	fmt.Println(cfg)
+	return nil
+}
+
+func TestRun(t *testing.T) {
+	cli := NewCLI("test", "0.0.1").
+		WithCommand(
+			NewCommand("subcmd1", action).
+				WithArgument(
+					NewIntCmdInput("positional1").AsArgument(),
+				).
+				WithArgument(
+					NewIntCmdInput("positional2").AsArgument(),
+				).
+				WithArgument(
+					NewIntCmdInput("positional3").AsArgument(),
+				).
+				WithFlag(
+					NewStringCmdInput("stringflag1").
+						WithDefault("default").
+						WithValidator(NewEnumValidator("default", "one", "two", "three")).
+						AsFlag().
+						FromEnv([]string{"STRINGFLAG1"}),
+				).
+				WithFlag(
+					NewCmdSliceInput[int]("sliceflag1").
+						WithDefault([]int{1, 2, 3}).
+						AsFlag(),
+				).
+				WithFlag(
+					NewCmdSliceInput[string]("sliceflag2").
+						WithDefault([]string{"one", "two", "three"}).
+						AsFlag(),
+				).
+				WithFlag(
+					NewCmdSliceInput[string]("sliceflag3").
+						WithDefault([]string{"one", "two", "three"}).
+						AsFlag(),
+				).
+				WithFlag(
+					NewCmdSliceInput[int]("sliceflag4").
+						WithDefault([]int{1, 2, 3}).
+						AsFlag(),
+				).
+				WithFlag(
+					NewIntCmdInput("intflag1").Required().AsFlag(),
+				),
+		)
+
+	ctx := context.Background()
+	err := cli.Run(ctx, []string{
+		"test",
+		"subcmd1",
+		"pos1",
+		"pos2",
+		"pos3",
+		// "--stringflag1", "stringflag1",
+		"--intflag1", "10",
+		"--sliceflag1", "4",
+		"--sliceflag1", "5",
+		"--sliceflag1", "6",
+		"--sliceflag2", "four",
+		"--sliceflag2", "five",
+		"--sliceflag2", "six",
+		"--sliceflag3", "seven,eight,nine",
+		"--sliceflag4", "7,8,9",
+	})
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+}
+
+func TestVersion(t *testing.T) {
+	cli := NewCLI("test", "0.0.1").
+		WithCommand(NewCommand("subcmd1", action)).
+		WithCommand(NewCommand("subcmd2", action))
+
+	ctx := context.Background()
+	err := cli.Run(ctx, []string{"test", "subcmd1", "--version"})
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+}
+
+func TestHelp(t *testing.T) {
+	cli := NewCLI("test", "0.0.1").
+		WithDescription("Test CLI").
+		WithLongDescription("This is a test CLI").
+		WithCommand(
+			NewCommand("subcmd1", action).
+				WithChildCommand(
+					NewCommand("subcmd11", action).
+						WithArgument(
+							NewStringCmdInput("arg1").
+								WithDefault("default").
+								WithValidator(
+									ComposeValidator(
+										NewStringLengthValidator(1, 10),
+										NewEnumValidator(
+											"one",
+											"two",
+											"three",
+										),
+									),
+								).
+								Required().
+								AsArgument(),
+						).
+						WithFlag(
+							NewIntCmdInput("intflag").
+								WithDefault(10).
+								WithValidator(
+									ComposeValidator(
+										NewIntRangeValidator(0, 100),
+										NewEnumValidator(1, 2, 3),
+									),
+								).
+								AsFlag(),
+						),
+				),
+		).
+		WithCommand(NewCommand("subcmd2", action))
+
+	t.Run("TopLevel", func(t *testing.T) {
+		ctx := context.Background()
+		err := cli.Run(ctx, []string{"test", "--help"})
+		if err != nil {
+			t.Fatalf("unexpected error: %v", err)
+		}
+	})
+
+	t.Run("SubCommand", func(t *testing.T) {
+		ctx := context.Background()
+		err := cli.Run(ctx, []string{"test", "subcmd1", "--help"})
+		if err != nil {
+			t.Fatalf("unexpected error: %v", err)
+		}
+	})
+
+	t.Run("SubCommandSubCommand", func(t *testing.T) {
+		ctx := context.Background()
+		err := cli.Run(ctx, []string{"test", "subcmd1", "subcmd11", "--help"})
+		if err != nil {
+			t.Fatalf("unexpected error: %v", err)
+		}
+	})
+}
